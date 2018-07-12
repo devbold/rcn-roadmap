d9da6a993f854fd0bc5c0614f8780b77e800300b
Ignacio <ignacio@ripiocredit.network>
2018-07-12 18:20:58 -0300

    Json structure

--- src/app/roadmap/roadmap-container/roadmap-container.component.ts
+++ src/app/roadmap/roadmap-container/roadmap-container.component.ts
@@ -7,12 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoadmapContainerComponent implements OnInit {
 
-  roadmap: any[] = [
-    {obj: 'This is an object of array[0]'},
-    {obj: 'This is an object of array[1]'},
-    {obj: 'This is an object of array[2]'},
-  ];
+  // roadmap: any[] = [
+  //   {obj: 'This is an object of array[0]'},
+  //   {obj: 'This is an object of array[1]'},
+  //   {obj: 'This is an object of array[2]'},
+  // ];
 
+  roadmap: any = [
+    {
+      "timelineComponent": [
+        {
+          "mainTitle": 'DApp SDK', "unabled": true,
+          "timeLine": true,
+          "title": 'DApp SDK'
+        }
+      ]
+    },
+    {
+      "timelineComponent": [
+        {
+          "mainTitle": 'DApp SDK', "unabled": true,
+          "timeLine": true,
+          "title": 'DApp SDK'
+        }
+      ]
+    }
+  ]
+  
   constructor() { }
 
   ngOnInit() {
