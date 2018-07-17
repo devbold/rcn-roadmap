4f09f45f4ed55416011f15e1800e3b05921d98d6
Ignacio <ignacio@ripiocredit.network>
2018-07-17 14:03:39 -0300

    Tooltips

--- src/app/roadmap/roadmap-container/roadmap-container.component.ts
+++ src/app/roadmap/roadmap-container/roadmap-container.component.ts
@@ -19,7 +19,7 @@ export class RoadmapContainerComponent implements OnInit {
             {href: 'https://github.com/ripio/rcn.loans', class: 'fab fa-github'},
           ],
           teamBlock: [
-            {href: 'https://github.com/ignacioperezroca', src: 'https://avatars3.githubusercontent.com/u/12101846?s=460&v=4' }
+            {href: 'https://github.com/ignacioperezroca', src: 'https://avatars3.githubusercontent.com/u/12101846?s=460&v=4', member: 'Ignacio Perez Roca' }
           ]
         },
         {
@@ -29,14 +29,14 @@ export class RoadmapContainerComponent implements OnInit {
             {href: 'https://github.com/ripio/rcn-api', class: 'fab fa-github'},
           ],
           teamBlock: [
-            {href: 'https://github.com/fernandezgonzalo', src: 'https://avatars1.githubusercontent.com/u/350436?s=460&v=4' }
+            {href: 'https://github.com/fernandezgonzalo', src: 'https://avatars1.githubusercontent.com/u/350436?s=460&v=4', member: 'Gonzalo Fernandez' }
           ]
         },
         {
           title: 'Python SDK',
           paragraph: 'The SDKs for Python/JAVA will facilitate the interaction between backends and mobile apps with an RCN Node',
           teamBlock: [
-            {href: 'https://github.com/fernandezgonzalo', src: 'https://avatars1.githubusercontent.com/u/350436?s=460&v=4' }
+            {href: 'https://github.com/fernandezgonzalo', src: 'https://avatars1.githubusercontent.com/u/350436?s=460&v=4', member: 'Gonzalo Fernandez' }
           ]
         },
         {
@@ -53,7 +53,7 @@ export class RoadmapContainerComponent implements OnInit {
           title: 'Claim Registry ERC939',
           paragraph: 'The ERC939 is our aproach to Identity and Scoring in the RCN Network. Agents will be making claims about the borrowers or cosigners, and those claims could be verified by possible lenders. https://github.com/ethereum/EIPs/issues/939',
           teamBlock: [
-            {href: 'https://github.com/Agusx1211', src: 'https://avatars0.githubusercontent.com/u/12701942?s=460&v=4' }
+            {href: 'https://github.com/Agusx1211', src: 'https://avatars0.githubusercontent.com/u/12701942?s=460&v=4', member: 'Agustin Aguilar' }
           ]
         },
         {
@@ -63,14 +63,14 @@ export class RoadmapContainerComponent implements OnInit {
             {href: 'https://github.com/ripio/rcn-network', class: 'fab fa-github'},
           ],
           teamBlock: [
-            {href: 'https://github.com/Agusx1211', src: 'https://avatars0.githubusercontent.com/u/12701942?s=460&v=4' }
+            {href: 'https://github.com/Agusx1211', src: 'https://avatars0.githubusercontent.com/u/12701942?s=460&v=4', member: 'Agustin Aguilar' }
           ]
         },
         {
           title: 'Loans Building Matrix',
           paragraph: 'Loans usually have negociation, a possible lender could propose diferent terms to lend the money, or a diferent Oracle. Flexible loans will be developed on a second layer, this layer will allow borrowers to request loans, listen for offers and accept the best one.',
           teamBlock: [
-            {href: 'https://github.com/Agusx1211', src: 'https://avatars0.githubusercontent.com/u/12701942?s=460&v=4' }
+            {href: 'https://github.com/Agusx1211', src: 'https://avatars0.githubusercontent.com/u/12701942?s=460&v=4', member: 'Agustin Aguilar' }
           ]
         }
       ]
@@ -87,7 +87,7 @@ export class RoadmapContainerComponent implements OnInit {
             {href: 'https://github.com/ripio/rcn-ramps', class: 'fab fa-github'},
           ],
           teamBlock: [
-            {href: 'https://github.com/rotcivegaf', src: 'https://avatars0.githubusercontent.com/u/7088778?s=460&v=4' }
+            {href: 'https://github.com/rotcivegaf', src: 'https://avatars0.githubusercontent.com/u/7088778?s=460&v=4', member: 'Victor Fage' }
           ]
         }
       ]
@@ -102,7 +102,7 @@ export class RoadmapContainerComponent implements OnInit {
             {href: 'https://github.com/ripio/rcn-ramps', class: 'fab fa-github'},
           ],
           teamBlock: [
-            {href: 'https://github.com/rotcivegaf', src: 'https://avatars0.githubusercontent.com/u/7088778?s=460&v=4' }
+            {href: 'https://github.com/rotcivegaf', src: 'https://avatars0.githubusercontent.com/u/7088778?s=460&v=4', member: 'Victor Fage' }
           ]
         }
       ]
@@ -114,7 +114,7 @@ export class RoadmapContainerComponent implements OnInit {
           title: 'Cosigner Refinancing',
           paragraph: 'Create a working example on how to implement a cosigner that refinaciates a debt, only if there is a common agreement between the parts.',
           teamBlock: [
-            {href: 'https://github.com/Agusx1211', src: 'https://avatars0.githubusercontent.com/u/12701942?s=460&v=4' }
+            {href: 'https://github.com/Agusx1211', src: 'https://avatars0.githubusercontent.com/u/12701942?s=460&v=4', member: 'Agustin Aguilar' }
           ]
         }
       ]
@@ -126,7 +126,7 @@ export class RoadmapContainerComponent implements OnInit {
           title: 'RCN Credit Lines',
           paragraph: 'Credit lines will allow lenders to offer money to borrowers or wallets. The lenders would be able to configure to who and how they want to lend, and the borrower could dispose of that tokens in any time',
           teamBlock: [
-            {href: 'https://github.com/Agusx1211', src: 'https://avatars0.githubusercontent.com/u/12701942?s=460&v=4' }
+            {href: 'https://github.com/Agusx1211', src: 'https://avatars0.githubusercontent.com/u/12701942?s=460&v=4', member: 'Agustin Aguilar' }
           ]
         }
       ]
